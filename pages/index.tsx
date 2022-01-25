24e5d4333cef693b7fcbe9634eff8037b2357c4a
Yuto Watanabe <yuto.w51942@gmail.com>
2022-01-25 12:01:47 +0900

    added react-password-checklist

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,21 @@
 import type { NextPage } from 'next'
+import React from 'react'
+import PasswordChecklist from "react-password-checklist"
 
 const Home: NextPage = () => {
+  const [password, setPassword] = React.useState('');
   return (
     <div>
+      <input onChange={(e) => {setPassword(e.target.value)}} />
 
+      <PasswordChecklist
+				rules={["minLength","specialChar","number","capital"]}
+				minLength={5}
+				value={password}
+				onChange={(isValid) => {
+          console.log(isValid);
+        }}
+			/>
     </div>
   )
 }
