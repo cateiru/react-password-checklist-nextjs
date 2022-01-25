2159fa8caf539afe584991bf81d20d5cdb5a3d11
Yuto Watanabe <yuto.w51942@gmail.com>
2022-01-25 12:09:05 +0900

    refactoring

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,16 +6,17 @@ const Home: NextPage = () => {
   const [password, setPassword] = React.useState('');
   return (
     <div>
-      <input onChange={(e) => {setPassword(e.target.value)}} />
+      <input type="password" onChange={(e) => {setPassword(e.target.value)}} />
+      <div>{password}</div>
 
       <PasswordChecklist
-				rules={["minLength","specialChar","number","capital"]}
-				minLength={5}
-				value={password}
-				onChange={(isValid) => {
-          console.log(isValid);
+        rules={["minLength","specialChar","number","capital"]}
+        minLength={5}
+        value={password}
+        onChange={(isValid) => {
+            console.log(isValid);
         }}
-			/>
+      />
     </div>
   )
 }
